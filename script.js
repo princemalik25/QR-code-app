f148f6e99fe69ec765aa63b33e5c1757c677063d
lakshyarohj <lakshyarohj12@gmail.com>
2025-02-27 19:05:05 +0530

    js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
     const url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 
-    const btn = document.querySelector(".btn")
+    const btn = document.querySelector("#btn")
 
-    btn.addEventListener("click", () => {
-    return fetchUrl();
-    })
-
+    
     async function fetchUrl(){
         try{
         const input = document.querySelector("#input").value;
@@ -15,11 +12,14 @@
             throw new Error(`Response status: ${response.status}`);
         }
         const qrCodeContainer = document.getElementById('qrCodeContainer');
-        qrCodeContainer.innerHTML = `<img src="${qrCodeUrl}" alt="QR Code">`;
+        qrCodeContainer.innerHTML = `<img src="${qrCodeUrl}" alt="QR Code" class="img">`;
         
         console.log(response);
-        }
-        catch(error) {
-            console.log(error);
-        }
     }
+    catch(error) {
+        console.log(error);
+    }
+}
+btn.addEventListener("click", async () => {
+    fetchUrl();
+})
